e1770b7168855fb39e441a94e720985c3e7aac1a
RobinShen <m18602031398@163.com>
2026-03-23 23:51:46 +0800

    Add debug-prisma.js

---
+++ debug-prisma.js
@@ -0,0 +1,16 @@
+const { Client } = require('ssh2');
+
+const conn = new Client();
+
+conn.on('ready', () => {
+  console.log('Connected');
+  conn.exec('docker logs --tail 50 evolution-whatsapp-backend-1', (err, stream) => {
+    stream.on('data', d => process.stdout.write(d));
+    stream.on('close', () => conn.end());
+  });
+}).connect({
+  host: '8.222.170.254',
+  port: 22,
+  username: 'root',
+  password: 'Teck0358'
+});
